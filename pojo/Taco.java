e9ec1dde2159078d1783d4a1171589c6b21f2c7e
Kruchyxe <krzysiekmilczanowski@interia.eu>
2022-02-03 13:33:42 +0100

    pojos 1st

---
+++ pojo/Taco.java
@@ -0,0 +1,18 @@
+package pl.kruchyxe.taco.pojo;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Taco {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+
+    }
+}
